225ee684accbd8110e6b5c2fda7814adaf93f887
Эльвина <elvinafayz@mail.ru>
2026-01-16 13:41:53 +0300

    adding fixtures for goods

--- goods/views.py
+++ goods/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from goods.models import Categories
 
 def catalog(request):
-     return render(request, 'goods/catalog.html')
+
+    categories = Categories.objects.all()
+    context = {'categories': categories}
+    return render(request, 'goods/catalog.html', context)
 
 def product(request):
     return render(request, 'goods/product.html')

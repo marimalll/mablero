5e189092cc77fe616e6466ffeccb1fbc00fa878d
Эльвина <elvinafayz@mail.ru>
2026-01-17 07:08:49 +0300

    pagination with GET params

--- goods/views.py
+++ goods/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from goods.models import Categories, Products
 
 def catalog(request, category_slug):
+    page = request.GET.get('page', 1)
     if category_slug == 'all':
         goods = Products.objects.all()
     else:
-        goods = get_object_or_404(Products.objects.filter(category__slug=category_slug))
-        print(goods)
-    context = {"goods": goods}
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+    paginator = Paginator(goods, 3)
+    current_page = paginator.page(page)
+    context = {"goods": current_page,
+               "slug_url": category_slug,}
     return render(request, 'goods/catalog.html', context)
 
 def product(request, product_slug):

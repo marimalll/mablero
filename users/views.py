092e3a3d99c809bfad41d93d7b1c22c8c33ea2b3
Эльвина <elvinafayz@mail.ru>
2026-01-23 11:49:48 +0300

    profile for, controller & notifications are ready

--- users/views.py
+++ users/views.py
@@ -1,10 +1,12 @@
+from django.contrib.auth.decorators import login_required
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.views.decorators.csrf import csrf_protect
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
+
 
 
 # Create your views here.
@@ -20,17 +22,29 @@ def login (request):
             # только если аутентификация прошла успешно - авторизация пользователя
             if user:
                 auth.login(request, user)
+                messages.success(request, f"{username}, Вы вошли в аккаунт")
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
     context = {'title': 'mablero - Авторизация',
                'form': form}
     return render(request, 'users/login.html', context)
+@login_required
 def logout (request):
+    messages.success(request, f"{request.user.username}, Вы вышли из аккаунта")
     auth.logout(request)
     return redirect(reverse('main:index'))
+@login_required
 def profile(request):
-    context = {'title': 'mablero - Кабинет'}
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f"Профиль успешно обновлен")
+            return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = ProfileForm(instance=request.user)
+    context = {'title': 'mablero - Кабинет', 'form': form}
     return render(request, 'users/profile.html', context)
 
 @csrf_protect
@@ -41,6 +55,7 @@ def registration(request):
             form.save()
             user = form.instance
             auth.login(request, user)
+            messages.success(request, f"{user.username}, Вы успешно зарегистрировались")
             return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserRegistrationForm()

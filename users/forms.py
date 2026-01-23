fe328dfdae3f841873ffd726d85126c9e8dccecf
Эльвина <elvinafayz@mail.ru>
2026-01-23 10:06:19 +0300

    registration form and controller are ready

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserChangeForm
 from users.models import User
 
 
@@ -19,3 +19,41 @@ class UserLoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ['username', 'password']
+
+
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from users.models import User
+
+
+class UserRegistrationForm(UserCreationForm):
+    # Поле для согласия с условиями
+    agree_to_terms = forms.BooleanField(
+        required=False,  # Важно: False, проверяем в clean
+        label='',
+        widget=forms.CheckboxInput(attrs={
+            'class': 'terms-checkbox',
+            'id': 'id_agree_to_terms'
+        })
+    )
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['agree_to_terms'].label = 'Я согласен с условиями использования'
+
+    def clean_agree_to_terms(self):
+        agreed = self.cleaned_data.get('agree_to_terms')
+        if not agreed:
+            raise forms.ValidationError(
+                '❌ Вы должны согласиться с условиями использования для регистрации',
+                code='terms_not_accepted'
+            )
+        return agreed
+
+    def clean(self):
+        cleaned_data = super().clean()
+        return cleaned_data
--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
 from django.http.response import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib import auth
+from django.views.decorators.csrf import csrf_protect
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 # Create your views here.
@@ -26,11 +27,23 @@ def login (request):
                'form': form}
     return render(request, 'users/login.html', context)
 def logout (request):
-    ...
+    auth.logout(request)
+    return redirect(reverse('main:index'))
 def profile(request):
     context = {'title': 'mablero - Кабинет'}
     return render(request, 'users/profile.html', context)
 
+@csrf_protect
 def registration(request):
-    context = {'title': 'mablero - Регистрация'}
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
+    context = {'title': 'mablero - Регистрация',
+               'form': form}
     return render(request, 'users/registration.html', context)

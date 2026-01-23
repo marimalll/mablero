087cffb96aed6029db78b3ebaead1a2ef5427977
Эльвина <elvinafayz@mail.ru>
2026-01-23 18:54:13 +0300

    Add to cart view, refractor included cart templates

--- users/views.py
+++ users/views.py
@@ -23,7 +23,8 @@ def login (request):
             if user:
                 auth.login(request, user)
                 messages.success(request, f"{username}, Вы вошли в аккаунт")
-                if request.POST.get('next', None):
+                redirect_page = request.POST.get('next', None)
+                if redirect_page and redirect_page != reverse('user:logout'):
                     return redirect(request.POST.get('next'))
                 return HttpResponseRedirect(reverse('main:index'))
     else:

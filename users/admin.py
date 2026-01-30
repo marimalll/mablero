f731ec1b08972f9341f4174aac3ad988c04b89ae
Эльвина <elvinafayz@mail.ru>
2026-01-30 15:08:39 +0300

    fix users admin inlines

--- users/admin.py
+++ users/admin.py
@@ -6,11 +6,9 @@ from users.models import User
 
 from carts.admin import CartTabAdmin
 
-from orders.admin import OrderTabulareAdmin
-
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'username', 'email', 'password']
 
-    inlines = [CartTabAdmin, OrderTabulareAdmin]
+    inlines = [CartTabAdmin]
